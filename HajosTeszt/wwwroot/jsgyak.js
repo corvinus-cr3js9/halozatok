ec38bd7827dd38f253a03005f7ec2cf148439555
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-03-18 18:30:28 +0100

    pascal 3szög-nem működik

---
+++ HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,16 @@
+﻿/*let hova = document.getElementById("ide")
+hova.innerHTML = ""
+for (var i = 0; i < 10; i++) {
+    var sor = document.createElement("div")
+    hova.appendChild(sor)
+    sor.innerText = i
+    sor.style.background = `rgb(${30 + (30 * i)},0,0)`
+    sor.classList.add("doboz")
+}*/
+/*var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}*/
